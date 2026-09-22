afa76f053ffe7267361886c0c71807e4ae58fd30
Santhiya <santhiya7454@gmail.com>
2026-09-22 11:18:52 +0530

    Fix Client Work Anchor link correctly

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
           <div className="hero-buttons">
 
             <a
-              href="#client-Work"
+              href="#client-work"
               className="main-button"
             >
               <span>View My Work</span>
